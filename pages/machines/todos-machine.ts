c6958ef269a2fe9bbcad95524c09df3f61ca1def
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-10-11 12:48:44 -0700

    type the new events and payload

--- pages/machines/todos-machine.ts
+++ pages/machines/todos-machine.ts
@@ -1,12 +1,14 @@
+import {stringify} from 'querystring'
 import {createMachine, assign} from 'xstate'
 
 export const todosMachineFactory = () =>
-  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FWwLQFsCGAxgBYCWAdmAHTkQkF7JgBiYyxAxBhdWQG6oBrKmgzZ8xclRp0GzVsQTl+9ZCVRkA2gAYAuolAAHTCVXr9IAB6IsAFgCMNyloAcdgKwB2OwCYAbFoc3dwAaEABPawBmb0c3by1fXzctAE4tGOc3GwBfbNCRTFxCUm5pFTk2InYwACca1BrKAwAbBgAzBpxKArFiyR5acpZKxT5UFTVNXXMjWBNJ8ysELF9vO0p4zPS3FN93LRtQiOX470pfZ2cPAPcUh1Tc-PRC8RKqGrA8CHZ1AFFaZAzYymMiLax2FLOSh2FxeBzXbweZzeNxHazeNaULyRLyJLI2FwpSKPEA9IoSbiQEyUACuBggsh+ZAAqvTZEC5iCwct3GccQ4bJF0nZInFomiEFpzh40s5fB5BSKAncUrk8iAyOg4OYya9+mVZMNiBz5mYkJZrKtHFdVt5nCkPPiJVhIqKsXYLn4CQS3PiSbq+twPl8TVzzUssBDIpQhSjvCldkK3HtvM7IjCNttUpFEgF4h5-c9ehSqLV6jVQwtw9ZPB5KG5ov47XYfDjDuFrM5ozYUjYgjFXX3os5C6JyW9KFTkLS2YxK2bQBGYo4UsvMt5Is4+zLUx3ltdKNutI2Fa4G95Ry9A6WAfPQdWThdKKubDZ15vtylnQ4zn3VmkAl8SI7i0NxL2LN4725FY30oG0MXtR1X2dAk61xGFnAOIUCRHNUgA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FWwLQFsCGAxgBYCWAdmAHTkQkF7JgBiYyxAxBhdWQG6oBrKmgzZ8xclRp0GzVsQTl+9ZCVRkA2gAYAuolAAHTCVXr9IAB6IsAFgBMANkoBGO84Cc7rQ+cOArAAc7s4ANCAAntYAzHYA7JTudgEOUTbuaZ52dgC+2WEimLiEpNzSKnJsROxgAE41qDWUBgA2DABmDTiUBWLFkjy05SyVinyoKmqauuZGsCaT5lYIWClRlHY2QW7ufnZ+6TZhkct23pRa7g4Osc5azgEPNn4OufnoheIlVDVgeBDs6gAorRkDNjKYyItrG4tC40s4oloAo5YllnLEjtC9i43A5kn5nDZnLsXnkQD0ihJuJATJQAK4GCCyAFkACqjNkYLmEKhJ2czkomy0Wn29yRfii6MxCFhN2SsVWaSiDjs7lisVyZLI6Dg5gpn36ZVkw2IXPmZiQlmsKScwSumyikoeUQC0qwjvcOJssUujiiPuCr3J716VO+vwgZp5lqWWFOfkoqVVSo8fmFWkOEWiCMoPgzPqyflifiLNiD+r61LqDSjCxj1kCnqLiLR1zxCLdyQSWg2yNVksuO3LIcpX0oNOQ9I5jFrFtAscRNnOjtuCLVOwebtisKJsRsNkRAURFyyw9Eo-6E9nkPryxiAWXkruUXXgVdWeWhLsgt824zG0CNIcjJCsw2vXkVgcWE7QcB0nSPd9jlsfkvS0REYjQ5tNWyIA */
   createMachine(
     {
       context: {
         todos: [] as string[],
         error: undefined as string | undefined,
+        todo: '',
       },
       tsTypes:
         {} as import('./todos-machine.typegen').Typegen0,
@@ -16,6 +18,14 @@ export const todosMachineFactory = () =>
             data: string[]
           }
         },
+        events: {} as
+          | {
+              type: 'onEdit'
+            }
+          | {
+              type: 'onUpdate'
+              value: string
+            },
       },
       predictableActionArguments: true,
       id: 'todos-machine',
