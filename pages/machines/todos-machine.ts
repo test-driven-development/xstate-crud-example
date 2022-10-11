b1e1505a176cbb17f9e957a383642411dacbd376
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-10-10 21:15:34 -0700

    introduce actions that can interrogate the event payload

--- pages/machines/todos-machine.ts
+++ pages/machines/todos-machine.ts
@@ -2,27 +2,36 @@ import {createMachine} from 'xstate'
 
 export const todosMachineFactory = () =>
   /** @xstate-layout N4IgpgJg5mDOIC5QBcD2FWwLQFsCGAxgBYCWAdmAHQBmYyxAkmRCQXspAMSpkDCqOAA4AbOmEShBmEshI8JIAB6IsARgDMAFkoAGLQHYAbOo3qAHAFZ9FgEwAaEAE8VqwzcqG3l9eps79AJw6hgC+IQ5oGNj4xORUtPRETCxsHBDcZACiAE7ZqNkKUrAycmQKyghYJqqUmmaGwToB+vomZuoWDs6VZvqUNsa9hgFmZqoWEzZh4SBk6HAKkZi4hKQUNHSMzKzskIXSsvJISipaFv0Bmjqqqjb6N4aBhl0uhmYehhZmAW+BN21hCLoZYxNbxTZEPbHIolI6gCpqVQBShmHTfdTWTxImx3F6VW6UCaaEYWczjCzNXyAkBLaKrOKUMC5fJQyQHUrlU4aSgaazjTS8v54tTuIkkskTZo2MzU2krWIUfbFQ5lY4I9TDC5XG53B5PYU2K4fCy3L6te6qaYhIA */
-  createMachine({
-    predictableActionArguments: true,
-    id: 'todos-machine',
-    initial: 'fetchIndicated',
-    schema: {
-      events: {} as
-        | {type: 'onComplete'; todos: string[]}
-        | {type: 'onError'; error: string},
-    },
-    states: {
-      fetchIndicated: {
-        on: {
-          onComplete: {
-            target: 'fetched',
-          },
-          onError: {
-            target: 'errored',
+  createMachine(
+    {
+      predictableActionArguments: true,
+      id: 'todos-machine',
+      initial: 'fetchIndicated',
+      schema: {
+        events: {} as
+          | {type: 'onComplete'; todos: string[]}
+          | {type: 'onError'; error: string},
+      },
+      states: {
+        fetchIndicated: {
+          on: {
+            onComplete: {
+              target: 'fetched',
+              actions: 'alertComplete',
+            },
+            onError: {
+              target: 'errored',
+            },
           },
         },
+        fetched: {},
+        errored: {},
       },
-      fetched: {},
-      errored: {},
     },
-  })
+    {
+      actions: {
+        alertComplete: (_, event) =>
+          alert(JSON.stringify(event)),
+      },
+    },
+  )
