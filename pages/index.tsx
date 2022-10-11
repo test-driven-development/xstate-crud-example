78fac2c63c9bdece87e3f90e943cb2c604b424eb
wil pannell <wil.pannell@wils-MacBook-Pro.local>
2022-10-10 19:06:18 -0700

    send event on mouseClick

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import {useMachine} from '@xstate/react'
 import type {NextPage} from 'next'
+import {helloMachine} from './machines/hello-machine'
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  const [state, send] = useMachine(helloMachine)
+  return (
+    <div>
+      {JSON.stringify(state.value)}
+      <button onClick={() => send('onMouseover')}>
+        Toggle
+      </button>
+    </div>
+  )
 }
 
 export default Home
